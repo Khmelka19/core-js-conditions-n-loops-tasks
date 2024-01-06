ee5649a1ee27334ab0c5972529440edc455e0ef2
Oxana Savianok <oxanas8833@gmail.com>
2024-01-06 23:03:39 +0300

    feat: add a solution to the isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -152,8 +152,14 @@ function convertNumberToString(/* numberStr */) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const n = Math.floor(str.length / 2);
+  for (let i = 0; i < n; i += 1) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
